8ec8ba5a7da1f0eb4ee42179d1db42ce2873e03c
rerickson <ryan.erickson4@gmail.com>
2024-05-01 14:58:01 -0700

    Get API working on WiFi Update android app to call API

---
+++ Android/app/src/main/java/com/example/mountainbiketrainer/network/TrainerAPIClient.kt
@@ -0,0 +1,37 @@
+import android.os.AsyncTask
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
+
+class ApiCaller {
+    private val client = OkHttpClient()
+
+    fun run() {
+        val request = Request.Builder()
+            .url("http://localhost:5029/api/Sessions")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+    }
+}
